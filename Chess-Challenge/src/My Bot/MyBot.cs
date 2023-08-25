1f697b22c89fa384b398ed40b62b8c1279b961bf
marcoferri42 <marcoferri462@gmail.com>
2023-08-25 04:19:02 +0200

    aa

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,17 +1,14 @@
 ﻿using ChessChallenge.API;
-using Microsoft.CodeAnalysis;
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Threading.Tasks.Dataflow;
 
 public class Node // classetta nodo custom
 {
     public bool isRoot = false;
     public int eval { get; set; }
     public Node root { get; set; }
-    public Move lastMove { get; set; }
+    public Move bestMove { get; set; }
 
     public List<Node> Children = new List<Node>();
 
@@ -25,25 +22,11 @@ public class Node // classetta nodo custom
     {
         this.root = root;
         this.eval = eval;
-        this.lastMove = move;
+        this.bestMove = move;
         this.isRoot = false;
     }
 }
 
-class NodeCompare : IComparer<Node>
-{
-    public int Compare(Node? x, Node? y)
-    {
-        if (x == null || y == null)
-        {
-            Console.WriteLine("ERROR COMPARE NULL");
-            return 0;
-        }
-
-        // Compare by the eval
-        return x.eval.CompareTo(y.eval);
-    }
-}
 
 
 public class MyBot : IChessBot
@@ -128,9 +111,10 @@ public class MyBot : IChessBot
         Node tree = new Node();
 
         if (board.GameMoveHistory.Length > 0)
-            Console.WriteLine(Evaluate(board, 0));
+            Console.WriteLine(board.GameMoveHistory.Length + "- " + Evaluate(board, 0));
 
-        for (int i = 1; i < 5; i++) // itereative deepening TODO da studiare bene ! 
+
+        for (int i = 1; i < 3; i++) // itereative deepening TODO da studiare bene ! 
         {
             tree = CreateTree(board, i, new Node());
 
@@ -139,92 +123,82 @@ public class MyBot : IChessBot
         }
 
         //Console.WriteLine(tree.Children[0].eval);             //<---------- evaluation print
-        return tree.Children[0].lastMove;
+        return tree.Children[0].bestMove;
     }
 
     private int Evaluate(Board board, int prevEval)
     {
         int score = 0, turn = board.IsWhiteToMove ? -1 : 1;
+        Move[] prevMoves = board.GameMoveHistory;
+
+        if (board.IsInCheckmate()){
+            return 1000000 * turn;
+        }
 
         if (seenPositions.ContainsKey(board.ZobristKey))
         {
             return seenPositions[board.ZobristKey];
         }
-        else
-        {
-            // EVAL checkmate
-            if (board.IsInCheckmate()){
-                return score + 1000000 * turn;
-            }
 
-/*
-            // EVAL draw
-            if (board.IsDraw()){
-                score += Math.Sign(prevEval) > 0 ? -10 : 10;
-            }
-*/
+        // possible moves
+        int currentPlayerMoves = board.GetLegalMoves().Length;
+        int currentPlayerCaptures = board.GetLegalMoves(true).Length;
 
-            // one move rule (piece weighted)
-            if (board.GameMoveHistory.Length > 3)
-            {
-                // EVAL onemoverule
-                PieceType previousMove = board.GameMoveHistory[board.GameMoveHistory.Length - 3].MovePieceType;
-                PieceType lastMove = board.GameMoveHistory.Last().MovePieceType;
-
-                if (previousMove == lastMove)
-                {
-                    score += 10 * turn ;
-                }
-
-            }
-
-            // material and positional value
-            foreach (PieceList list in board.GetAllPieceLists())
-            {
-                foreach (Piece piece in list)
-                {
-                    // EVAL material
-                    score += values[piece.PieceType] * (piece.IsWhite ? 1 : -1); // material value
-
-                    (int x, int y) coords = getPieceCoords(piece);
-                    
-
-                    // flippa rows se nero
-                    int [,] pos = positional[piece.PieceType];
-                    
-                    if (piece.IsWhite)
-                        Reverse2DArray(pos);
-
-                    // EVAL positional
-                    score += pos[coords.x, coords.y] * (piece.IsWhite ? 1 : -1) / 2;
-
-                }
-
-            }
-
-/*
-            // possible moves incentive
-            int currentPlayerMoves = board.GetLegalMoves().Length;
+        if (board.TrySkipTurn()){
+            int opponentMoves = board.GetLegalMoves().Length;
             
-            if (board.TrySkipTurn()){
-                int opponentMoves = board.GetLegalMoves().Length;
-                
-                int moveCountDifference = currentPlayerMoves - opponentMoves;
-                // EVAL POSSIBLE MOVES
-                score += (moveCountDifference * turn)/4;
+            // EVAL POSSIBLE MOVES
+            score += (currentPlayerMoves > opponentMoves ? 1 : 0) * turn;  
 
-                board.UndoSkipTurn();
+            int opponentCaptures = board.GetLegalMoves(true).Length;
+
+            // EVAL CAPTURES
+
+            score += (currentPlayerCaptures > opponentCaptures ? 1 : 0) * turn;
+
+            board.UndoSkipTurn();
+        }
+
+        // one move rule (piece weighted)
+        if (prevMoves.Length >= 3)
+        {
+            // EVAL onemoverule
+            if (prevMoves[^3].MovePieceType == prevMoves[^1].MovePieceType)
+            {
+                score += values[prevMoves[^1].MovePieceType]/5 * (-1*turn) ;
             }
-*/
-
-
-
-            // add position to table
-            seenPositions.Add(board.ZobristKey, score);
-
-            return score;
 
         }
+
+        // material and positional value
+        foreach (PieceList list in board.GetAllPieceLists())
+        {
+            foreach (Piece piece in list)
+            {
+                score += values[piece.PieceType] * (piece.IsWhite ? 1 : -1); // material value
+
+                (int x, int y) coords = getPieceCoords(piece);
+
+                int [,] pos = positional[piece.PieceType];
+                
+                // flippa rows se nero
+                if (!piece.IsWhite)
+                {
+                    pos = Reverse2DArray(pos);
+                }
+
+                score += pos[coords.x, coords.y] * (piece.IsWhite ? 1 : -1);
+                
+                //Console.WriteLine(pos[coords.x, coords.y] + " , " + (piece.IsWhite ? "w":"b"));
+            }
+
+        }
+
+
+        // add position to table
+        seenPositions.Add(board.ZobristKey, score);
+
+        return score;
     }
 
     private (int, int) getPieceCoords(Piece piece)
@@ -243,9 +217,6 @@ public class MyBot : IChessBot
 
     private Node CreateTree(Board board, int depth, Node rootNode)
     {
-        if (totMoves > 200000){
-            return rootNode;
-        }
         if (depth > 0)
         {
             Move[] moves = board.GetLegalMoves();
@@ -253,11 +224,13 @@ public class MyBot : IChessBot
             moves = FilterMoves(moves, board);
 
             totMoves += moves.Length;
+            
+            int eval = 0;
 
             foreach (Move move in moves)
             {
                 board.MakeMove(move);
-                int eval = Evaluate(board, rootNode.eval);
+                eval = Evaluate(board, rootNode.eval);
 
                 rootNode.Children.Add(
                     CreateTree(board, depth - 1, new Node(rootNode, eval, move)) // recursive call for children
@@ -266,15 +239,10 @@ public class MyBot : IChessBot
                 board.UndoMove(move);
             }
 
-            if (rootNode.Children.Count > 0)
+            if (moves.Length > 0 && rootNode.Children.Count == moves.Length)
             {
-                rootNode.Children.Sort(new NodeCompare()); // compares in ascending eval order  --> white best move first
-
-                if (board.IsWhiteToMove) // black best move first
-                {
-                    rootNode.Children.Reverse();
-                }
-
+                rootNode.Children = SortEval(rootNode.Children, !board.IsWhiteToMove); // true -> ascending order >> black -> asc, white -> desc
+                //Console.WriteLine(rootNode.Children[0].eval + " " + board.IsWhiteToMove);
                 rootNode.eval = rootNode.Children[0].eval;
             }
 
@@ -295,20 +263,25 @@ public class MyBot : IChessBot
 
             board.MakeMove(move);
 
-            if(nMoves < 4) // mosse opening ARBITRARIO A CAZZO TODO CAMBIA
-            {
-                if (move.MovePieceType == PieceType.Pawn){
-                    Hpriority.Add(move);
-                }
-            }
-
             if (board.IsInCheckmate())
             {
                 board.UndoMove(move);
                 return new Move[] { move }; ;
             }
 
-            if (move.IsPromotion || move.IsCastles || move.IsCapture)
+            if (nMoves < 5) // mosse opening
+            {
+                if (move.MovePieceType == PieceType.Pawn){
+                    Hpriority.Add(move);
+                }
+            }
+
+
+            if (board.SquareIsAttackedByOpponent(move.TargetSquare)){
+                Lpriority.Add(move);
+            }
+
+            if (move.IsPromotion || move.IsCastles)
             {
                 Hpriority.Add(move);
             }
@@ -327,7 +300,7 @@ public class MyBot : IChessBot
     }
 
     // GPT :)
-    static void Reverse2DArray(int[,] array)
+    private int[,] Reverse2DArray(int[,] array)
     {
         int rows = array.GetLength(0);
         int cols = array.GetLength(1);
@@ -341,20 +314,19 @@ public class MyBot : IChessBot
                 array[rows - i - 1, j] = temp;
             }
         }
+        return array;
     }
+
+
+    // tree eval sort
+    static List<Node> SortEval(List<Node> nodes, bool ascending = true)
+    {
+        // compara
+        Comparison<Node> comparison = (node1, node2) => ascending ? node1.eval.CompareTo(node2.eval) : node2.eval.CompareTo(node1.eval);
+
+        // sorta
+        nodes.Sort(comparison);
+        return nodes;
+    }
+
 }
-/*
-TODO:
-
-1. Piece Move Order
-2. Clean Up EVERYTHING (varnames etc)
-
-
-
-
-
-
-
-
-
-*/

beafb104d09d5c7941fccc2812a0d08d72ca25cc
matifema <marcoferri462@gmail.com>
2023-12-29 15:39:36 +0100

    cli

--- Chess-Challenge/src/My Bot/Cli.cs
+++ Chess-Challenge/src/My Bot/Cli.cs
@@ -20,36 +20,50 @@ internal class Cli
             switch (input)
             {
                 case "uci":
-                    Console.WriteLine("id command");
-                    Console.WriteLine("option commands");
-                    Console.WriteLine("uciok command");
+                    Console.WriteLine("id name OctoBot");
+                    Console.WriteLine("id author joe");
+                    Console.WriteLine("option");
+                    Console.WriteLine("uciok");
                     break;
 
                 case "isready":
-                    Console.WriteLine("readyok command");
+                    Console.WriteLine("readyok");
                     break;
 
                 case "ucinewgame":
+                    // reset board and data from search
                     break;
-
+                case "stop":
+                    // stop calculation
+                    break;
+                case "quit":
+                    // quit engine
+                    break;
                 default:
                     break;
             }
-            if (input.Contains("position"))
+            if (input.Contains("position") && input.Contains(" ") && input.Count()> 10)
             {
                 var payload = input[9..].Split(" ");
 
                 var fen = payload[1];
                 var moves = payload[3..];
-                if (payload[2].ToString() == "startpos")
+                if (payload[2].ToString() != "move")
                 {
-
                     moves = payload[4..];
                 }
 
                 Console.WriteLine(fen);
                 Console.WriteLine(moves);
                 Array.ForEach(moves, Console.WriteLine);
+            }
+
+
+            if (input.Count() >= 1 && input[..2].ToString() == "go")
+            {
+                // start engine
+                Console.WriteLine("start");
+                Console.WriteLine("info depth 1 score cp -1 time 10 nodes 26 nps 633 pv e7e6");
             }
         }
     }
